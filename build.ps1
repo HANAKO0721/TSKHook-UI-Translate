c2bcb153920ad5572b34c2cb2ef16a515d34ff7d
Michael <85883922+HANAKO0721@users.noreply.github.com>
2026-09-24 21:38:53 +0800

    Delete build.ps1

--- build.ps1
+++
@@ -1,5 +0,0 @@
-param([Parameter(Mandatory = $true)][string]$GamePath)
-$ErrorActionPreference = 'Stop'
-$env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
-& dotnet build (Join-Path $PSScriptRoot 'src\TSKHook.UI\TSKHook.UI.csproj') -c Release --nologo "-p:GamePath=$GamePath"
-if ($LASTEXITCODE -ne 0) { throw 'Plugin build failed.' }
